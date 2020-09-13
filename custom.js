5cd5aedcb4c815fe7b525f8f7aaf63d6a4f03cab
Lahulesh Chourasiya <lahuleshchourasiya2000@gmail.com>
2020-09-13 17:10:49 +0530

    my PSD TO HTML

---
+++ custom.js
@@ -0,0 +1,15 @@
+
+
+    var swiper = new Swiper('.swiper-container', {
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        // Enable debugger
+        debugger: true,
+      });
+
